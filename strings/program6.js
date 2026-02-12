5c744f8f2d58e5d533eb90b86c86d3208d992726
mythrimannuru123 <mythrireddy345@gmail.com>
2026-02-12 16:55:54 +0530

    strings

---
+++ strings/program6.js
@@ -0,0 +1,6 @@
+function contains(str, substr) {
+    return str.includes(substr);
+}
+
+console.log(contains("Welcome to codingjava", "coding")); // true
+console.log(contains("Welcome to coding", "python"));
---
+++ strings/program8.js
@@ -0,0 +1,5 @@
+function extractSubstring(str, start, end) {
+    return str.substring(start, end);
+}
+
+console.log(extractSubstring("mythri reddy", 0, 6)); 
